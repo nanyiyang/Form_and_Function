6ac8144984aec7dc849ed926892e8ae51d7205e6
hoyishian <ysho2@illinois.edu>
2018-12-02 15:59:50 -0600

    Changes to Home Screen - Store values for Radio Buttons (Gender and Preference Type) - Store as Strings

--- app/src/main/java/com/example/ryann/formandfunction/HomeScreen.java
+++ app/src/main/java/com/example/ryann/formandfunction/HomeScreen.java
@@ -2,14 +2,27 @@ package com.example.ryann.formandfunction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class HomeScreen extends AppCompatActivity {
+
+    /*** To check if either of the Radio Buttons for gender has been checked. */
+    private boolean genderCheckField = false;
+
+    /*** To check if either of the Radio Buttons for preference type has been checked. */
+    private boolean preferenceCheckField = false;
+
+    public String gender;
+
+    public String preference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,36 @@ public class HomeScreen extends AppCompatActivity {
                 startActivity(new Intent(HomeScreen.this, MainActivity.class));
             }
         });
+    }
+
+
+    public void buttonPreference(View view) {
+        boolean check = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.genderButtonMale:
+                if (check)
+                    gender = "Male";
+                    genderCheckField = true;
+                break;
+
+            case R.id.genderButtonFemale:
+                if (check)
+                    gender = "Female";
+                    genderCheckField = true;
+                break;
+        }
+        switch (view.getId()) {
+            case R.id.styleButtonSmart:
+                if (check)
+                    preference = "Smart";
+                    preferenceCheckField = true;
+                break;
+            case R.id.styleButtonStreet:
+                if (check)
+                    preference = "Street";
+                    preferenceCheckField = true;
+                break;
+        }
     }
 
 }
