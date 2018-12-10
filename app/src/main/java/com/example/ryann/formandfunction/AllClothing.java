15031f534625f1ed6f9378a490cf16a85dbb7a9a
nanyiyang <ryan.nanyi@gmail.com>
2018-12-10 12:02:39 -0600

    Working on AllClothing

--- app/src/main/java/com/example/ryann/formandfunction/AllClothing.java
+++ app/src/main/java/com/example/ryann/formandfunction/AllClothing.java
@@ -11,91 +11,41 @@ import android.widget.LinearLayout;
 
 public class AllClothing extends AppCompatActivity {
 
-//    public static double temperature;
-//    public static String style;
-//    private static String gender;
-//    private static double precipitation;
-//
-//    // Setter for Gender
-//    public static void setGender(String newGender) {
-//        gender = newGender;
-//    }
-//
-//    // Setter for Style Preference
-//    public static void setStyle(String newStyle) {
-//        style = newStyle;
-//    }
-//
-//    // Setter for Temperature where the input comes from the API Call.
-//    public static void setTemperature(double newTemperature) {
-//        temperature = newTemperature;
-//    }
-//
-//    // Setter for Precipitation where the input comes from the API Call. Yet to be coded for MainActivity.
-//    public static void setPrecipitation(double newPrecipitation) {
-//        precipitation = newPrecipitation;
-//    }
-//
-//    public static String getStyle() {
-//        if (style == null) {
-//            return null;
-//        }
-//        return style;
-//    }
-//
-//    public static String getGender() {
-//        if (gender == null) {
-//            return null;
-//        }
-//        return gender;
-//    }
-//
-//    public static double getTemperature() {
-//        return temperature;
-//    }
-//
-//    public static double getPrecipitation() {
-//        return precipitation;
-//    }
 
     // Tentatively set the weather where you require coat/ winter jacket is below 45.0 Fahrenheit
     public void getCorrectClothing() {
+        LinearLayout maleStreet = (LinearLayout) findViewById(R.id.maleStreet);
+        LinearLayout maleSmart = (LinearLayout) findViewById(R.id.maleSmart);
+
         try {
             // Selects clothing suitable for this weather.
             if (MainActivity.globalTemperature < 45) {
                 if (MainActivity.globalPrecipitation > 0.0) {
                     // Returns clothing that is suitable for temperature below 45.0F and for Rainy weather
                     if (HomeScreen.globalPreference.equals("Smart") && HomeScreen.globalGender.equals("Male")) {
-                        LinearLayout maleStreet = (LinearLayout) findViewById(R.id.maleStreet);
-                        maleStreet.setVisibility(View.GONE);
+                        maleSmart.setVisibility(View.VISIBLE);
 
                     } else if (HomeScreen.globalPreference.equals("Smart") && HomeScreen.globalGender.equals("Female")) {
 
                     } else if (HomeScreen.globalPreference.equals("Street") && HomeScreen.globalGender.equals("Male")) {
-                        LinearLayout maleSmart = (LinearLayout) findViewById(R.id.maleSmart);
-                        maleSmart.setVisibility(View.GONE);
+                        maleStreet.setVisibility(View.VISIBLE);
 
                     } else if (HomeScreen.globalPreference.equals("Street") && HomeScreen.globalGender.equals("Female")) {
 
-                    } else {
-                        throw new NullPointerException();
                     }
                 } else {
                     // Returns clothing that is suitable for temperature below 45.0F and for Sunny weather
                     if (HomeScreen.globalPreference.equals("Smart") && HomeScreen.globalGender.equals("Male")) {
-                        LinearLayout maleStreet = (LinearLayout) findViewById(R.id.maleStreet);
-                        maleStreet.setVisibility(View.GONE);
+                        Log.d("Test", "HELLO IT REACHED HERE");
+                        maleSmart.setVisibility(View.VISIBLE);
 
                     } else if (HomeScreen.globalPreference.equals("Smart") && HomeScreen.globalGender.equals("Female")) {
 
                     } else if (HomeScreen.globalPreference.equals("Street") && HomeScreen.globalGender.equals("Male")) {
-                        LinearLayout maleSmart = (LinearLayout) findViewById(R.id.maleSmart);
-                        maleSmart.setVisibility(View.GONE);
+                        maleStreet.setVisibility(View.VISIBLE);
 
                     } else if (HomeScreen.globalPreference.equals("Street") && HomeScreen.globalGender.equals("Female")) {
 
-                    } else {
-                        throw new NullPointerException();
                     }
                 }
 
@@ -111,8 +61,6 @@ public class AllClothing extends AppCompatActivity {
 
                     } else if (HomeScreen.globalPreference.equals("Street") && HomeScreen.globalGender.equals("Female")) {
 
-                    } else {
-                        throw new NullPointerException();
                     }
                 } else {
                     // Returns clothing that is suitable for temperature above 45.0F and for Sunny weather
@@ -124,8 +72,6 @@ public class AllClothing extends AppCompatActivity {
 
                     } else if (HomeScreen.globalPreference.equals("Street") && HomeScreen.globalGender.equals("Female")) {
 
-                    } else {
-                        throw new NullPointerException();
                     }
                 }
             }
@@ -138,17 +84,10 @@ public class AllClothing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getCorrectClothing();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_clothing);
+        getCorrectClothing();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
     }
-
-
-
-
-
 }
